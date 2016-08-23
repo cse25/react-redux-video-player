dc44eeccf07168dd7cb1f916ccc55fcc175bebb1
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-08-23 09:21:17 -0700

    create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar.js';
+
+
+const App = () => {
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
