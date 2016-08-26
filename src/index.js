639e5feeadebed6aab8834fbcd1923ea305e6776
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-08-25 21:13:02 -0700

    move YTSearch inside App

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar.js';
 
 
-YTSearch({'key': API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-})
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({'key': API_KEY, term: 'surfboards'}, function(videos) {
+      this.setState({ videos: videos });
+    });
+  }
+
   render() {
     return (
       <div>
