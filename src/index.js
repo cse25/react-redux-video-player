a5b6a214e68acae35960d296cf72bf2703bef20e
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-08-31 19:18:03 -0700

    throttle videoSearch function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -15,10 +16,10 @@ class App extends Component {
       selectedVideo: null
     };
 
-    this.searchVideos('surfboards');
+    this.videoSearch('surfboards');
   }
 
-  searchVideos(term) {
+  videoSearch(term) {
     YTSearch({'key': API_KEY, term: term}, (videos) => {
       this.setState({ 
         videos: videos,
@@ -28,9 +29,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.searchVideos(term)} />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
